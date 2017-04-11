5d4f25f6d954ae7759c0da0587f22982e8778bc3
Mathias Schwaninger <M.Schwaninger@seeburger.de>
2017-04-11 09:34:27 +0200

    Add new class for initialize with JBoss Web host container

---
+++ pax-web-tomcat/src/main/java/org/ops4j/pax/web/service/tomcat/JBossWebBridge.java
@@ -0,0 +1,29 @@
+/*
+ * JBossWebBridge.java
+ *
+ * created at 11.04.2017 by Schwaninger <m.schwaninger@seeburger.de>
+ *
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package org.ops4j.pax.web.service.tomcat;
+
+import org.apache.catalina.core.ContainerBase;
+import org.ops4j.pax.web.service.spi.ServerControllerFactory;
+import org.ops4j.pax.web.service.tomcat.internal.TomcatServerControllerFactory;
+import org.ops4j.pax.web.service.tomcat.internal.TomcatServerFactory;
+import org.ops4j.pax.web.service.tomcat.internal.TomcatServerStateFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+public class JBossWebBridge
+{
+    public void start(ContainerBase host)
+    {
+        BundleContext context = FrameworkUtil.getBundle(getClass()).getBundleContext();
+        ServerControllerFactory factory = TomcatServerControllerFactory.newInstance(TomcatServerStateFactory.newInstance(new TomcatServerFactory(host)));
+        context.registerService(ServerControllerFactory.class, factory, null);
+    }
+}
+
+
+
